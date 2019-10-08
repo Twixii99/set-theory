8001cb5f6fa2b92bc9cc662346ea11e8bc6ebdf6
Mahmoud kamal <42426976+lilkamal@users.noreply.github.com>
2019-10-08 21:48:59 +0200

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -2,17 +2,37 @@
 #include <algorithm>
 #include <iomanip>
 #include <vector>
+#include <stdlib.h>
 using namespace std;
 
 int iSize = 0, iSub = 0, iEsize = 0;
 bool arr[100][100];
+
+void intersection(int firstSet,int secSet,string sUniversal []){
+    for(int i=0;i<iSize;i++)
+        if(arr[firstSet][i] && arr[secSet][i]) cout <<sUniversal[i] <<" ";
+    cout<<"\n\n";
+}
+
+void _union (int firstSet,int secSet,string sUniversal []){
+    for(int i=0;i<iSize;i++)
+        if(arr[firstSet][i] || arr[secSet][i]) cout <<sUniversal[i] <<" ";
+    cout<<"\n\n";
+}
+
+void difference (int firstSet,int secSet,string sUniversal []){
+    for(int i=0;i<iSize;i++)
+        if(arr[firstSet][i] && !arr[secSet][i]) cout <<sUniversal[i] <<" ";
+    cout<<"\n\n";
+}
+
 int main(int argc, char const *argv[])
 {
-    cout << setw(5) << "Starting programm...\n";
-
     // UNIVERSAL SECTION TOKEN
-    cout << "Enter the number of elements in the universel set -> ";
-    cin >> iSize;
+    do {
+        cout << "Enter the number of elements in the universel set -> ";
+        cin >> iSize;
+    } while(iSize < 0);
     string sUniversal[iSize];
     cout << "Now enter your universal elements: \n";
     for(int i = 0; i < iSize; ++i)
@@ -20,17 +40,20 @@ int main(int argc, char const *argv[])
     sort(sUniversal, sUniversal + iSize);
 
     // SUBSETS SECTION
-    cout << "Enter your number of subsets -> ";
-    cin >> iSub;
-    cout << "\n";
+    do {
+        cout << "Enter your number of subsets -> ";
+        cin >> iSub;
+    } while(iSub <= 0);
     for(int i = 0; i < iSize; ++i)
         arr[0][i] = 1;
     for(int i = 0; i < iSub; ++i) {
+    	cin.ignore(10, '\n');
+    	cin.clear();
         iEsize = 0;
         cout << "Enter your subset size -> ";
         cin >> iEsize;
-        while(iEsize > iSize) {
-            cout << "A*a nt htst3bt -> ";
+        while(iEsize > iSize || iEsize < 0) {
+            cout << "WARNING << ENTER A SUTIBLE SUBSET SIZE -> ";
             cin >> iEsize;
         }
         string sTemp[iEsize];
@@ -45,8 +68,8 @@ int main(int argc, char const *argv[])
             else
                 arr[i+1][x] = 1, ++x, ++k;
         if(k != iEsize) {
-            cout << "A*a tany -> there is an element isn't exist in the universal set bro7 omk\n";
-            cout << "Enter your fucken subset again\n";
+            cout << "WARNING << there is an element isn't exist in the universal set\n";
+            cout << "Enter your subset again please:\n";
             for(x = 0; x < iSize; ++x)
                 arr[i+1][x] = 0;
             --i;
@@ -55,24 +78,30 @@ int main(int argc, char const *argv[])
     }
 
     // OPERATIONS SECTION
+    cout << "Enter your exp in a this form \"set1 ope set2\"\n";
+    cout << "+ for union <-> ^ for intersect <-> - for diff\n";
     while(1) {
-        string exp = "";
-        cout << "/**\nEnter YOUR EXPRESSION seperated by spaces. <<But take care>> \n enter \"^\" for \"intersection\" \n \"+\" for \"union\" \n \"-\" for \"diff\"\n";
-        cout << "And your subSet are numbered from 1 to n containing universal as number 0\n";
+    	// if the user entered elements > the number of them...
+    	cin.clear();
+    	cin.ignore(10, '\n');
         int iOne, iTwo;
         char x;
-        cin >> iOne >> x >> iTwo;
+        do {
+            cout << "Your subSet are numbered from 1 to n containing universal as number 0\n";
+            cin >> iOne >> x >> iTwo;
+        } while(iOne < 0 || iOne > iSub || iTwo < 0 || iTwo > iSub);
         switch(x) {
             case '^':
-                intersection(iOne, iTow);
+                intersection(iOne, iTwo, sUniversal);
                 break;
             case '+':
-                _union(iOne, iTwo);
+                _union(iOne, iTwo, sUniversal);
                 break;
-            default:
-                difference(iOne, iTow);
+            case '-':
+                difference(iOne, iTwo, sUniversal);
+            default :
+            	continue;
         }
-
     }
     return 0;
 }
